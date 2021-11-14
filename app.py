fc30efbd6d5c52f19b6cd13b4e404556a4192d58
JesperKauppinen <jesperkauppinen@gmail.com>
2021-11-14 22:57:32 +0200

    removed unused code for heroku

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from __init__ import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=4999, debug=True)
+    app.run()
--- views.py
+++ views.py
@@ -1,12 +1,6 @@
 from flask import Blueprint, render_template, request
 
-
-from __init__ import db
-from models import Vote, User
-
-
 """     This file handle showing landing page:  """
-
 
 views = Blueprint('views', __name__)
 
