5ee7c3e931e3add5f30f006f599bc170871df14a
JesperKauppinen <jesperkauppinen@gmail.com>
2021-11-15 10:28:43 +0200

    check mobile users

--- views.py
+++ views.py
@@ -9,5 +9,7 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET'])
 def home():
     if request.method == "GET":
-        return render_template("mobile.html")
-        # return render_template("index.html")
+        user_agent = request.headers.get('User-Agent')
+        if 'mobile' in user_agent.lower():
+            return render_template("mobile.html")
+        return render_template("index.html")
