15c99f8c1d29a70487455998a498c6bb67eb2691
JesperKauppinen <jesperkauppinen@gmail.com>
2021-11-16 15:15:46 +0200

    added post request handling

--- views.py
+++ views.py
@@ -6,12 +6,16 @@ views = Blueprint('views', __name__)
 
 
 # Landing Page
-@views.route('/', methods=['GET'])
+@views.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == "GET":
-        user_agent = request.headers.get('User-Agent')
-        if 'mobile' in user_agent.lower():
-            flash(f"Hello Mobile user, Welcome to use pomodoro clock. Hopefully you have productive time.", "notify")
-            return render_template("mobile.html")
-        flash(f"Hello Desktop user, Welcome to use pomodoro clock. Hopefully you have productive time.", "info")
-        return render_template("index.html")
+    user_agent = request.headers.get('User-Agent')
+
+    if request.method == "POST":
+        pass
+
+    if 'mobile' in user_agent.lower():
+        flash(f"Hello Mobile user, Welcome to use pomodoro clock. Hopefully you have productive time.", "notify")
+        return render_template("mobile.html")
+
+    flash(f"Hello Desktop user, Welcome to use pomodoro clock. Hopefully you have productive time.", "info")
+    return render_template("index.html")
