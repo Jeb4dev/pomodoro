33e39ffe912e3939bd8be4085eb972e050349e2e
JesperKauppinen <jesperkauppinen@gmail.com>
2021-11-16 14:41:07 +0200

    mobile notifications

--- views.py
+++ views.py
@@ -11,8 +11,7 @@ def home():
     if request.method == "GET":
         user_agent = request.headers.get('User-Agent')
         if 'mobile' in user_agent.lower():
-            # TODO: mobile notifications
-            # flash(f"Hello Mobile user, Welcome to use pomodoro clock. Hopefully you have productive time.", "info")
+            flash(f"Hello Mobile user, Welcome to use pomodoro clock. Hopefully you have productive time.", "notify")
             return render_template("mobile.html")
         flash(f"Hello Desktop user, Welcome to use pomodoro clock. Hopefully you have productive time.", "info")
         return render_template("index.html")
