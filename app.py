b7ac9e1c4e74aaead42346ce74e051bd3047a825
JesperKauppinen <jesperkauppinen@gmail.com>
2021-11-18 15:34:28 +0200

    removed localhost

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app, socketio = create_app()
 
 if __name__ == '__main__':
     # socketio.run(app,  debug=True, host="0.0.0.0", port=4999)
-    socketio.run(app, host="0.0.0.0", port=4999)
+    socketio.run(app)
