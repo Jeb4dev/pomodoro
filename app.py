2518efd82442f7868c85fdfc27026074cd63cf7a
Jeb <jesperkauppinen@gmail.com>
2022-08-28 20:01:19 +0300

    allow unsafe socket server for development

--- app.py
+++ app.py
@@ -5,4 +5,10 @@ from __init__ import create_app
 app, socketio = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app,  debug=environ.get("FLASK_DEBUG") or True, host="0.0.0.0", port=environ.get("BIND_PORT") or 8000)
+    socketio.run(
+        app,
+        debug=environ.get("FLASK_DEBUG") or True,
+        host="127.0.0.1",
+        port=environ.get("BIND_PORT") or 8000,
+        allow_unsafe_werkzeug=True
+    )
